b4a03493dbd82d789cc961420dcffc84fd8a212d
rzzldzzldesign <letsrazzledazzle@gmail.com>
2019-12-09 07:46:22 +1300

    Tweaked ways to make a function

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -1,4 +1,4 @@
-// ?Regular Function Declaration (IS HOISTED):
+// ? Regular Function Declaration (IS HOISTED):
 // function doctorize(firstName = 'Bob') {
 //   return `Dr. ${firstName}`;
 // }
@@ -15,24 +15,24 @@
 // };
 // console.log(doctorize('Jay'));
 
-// ?Arrow Functions
+// ? Arrow Functions
 const inchToCM = inches => inches * 2.54;
 
 console.log(inchToCM(3));
 
-// ?TEST ARROW FUNCTION
+// ? TEST ARROW FUNCTION
 const doctorize = firstName => `Dr. ${firstName}`;
 const yell = firstName => firstName.toUpperCase();
 
 console.log(yell(doctorize('Jay')));
 
-// ?TEST AGAIN
+// ? TEST AGAIN
 const add = (a, b = 3) => a + b;
 
-console.log(add(7, 20));
+console.log(add(7, 11));
 
 /* eslint-disable */
-// ?Function creating an object
+// ? Function creating an object
 // function makeABaby(firstName, lastName) {
 //   const baby = {
 //     name: `${firstName} ${lastName}`,
@@ -41,19 +41,19 @@ console.log(add(7, 20));
 //   return baby
 // }
 
-// ?Convert to arrow function?
+// ? Convert to arrow function?
 const makeABaby = (firstName, lastName) => ({ name: `${firstName} ${lastName}`, age: 0 })
 
 console.log(makeABaby('Bing', 'Bong'));
 
-// ?IIFE Function
+// ? IIFE Function
 // Immediately Invoked Function Expresiion [Wrapped in () followed by (argument)]
 (function(howCool) {
-    console.log(`You are cool X${howCool}`)
-    return howCool
-})(9009)
+    const thisCool = console.log(`You are cool X${howCool}`)
+    return thisCool
+})(9000)
 
-// ?Methods!! [Function that lives inside of an Object]
+// ? Methods!! [Function that lives inside of an Object]
 const jay = {
     name: 'Jay',
     // V This is the method within the jay object
@@ -70,8 +70,8 @@ const jay = {
     }
 }
 
-// ?Callback functions
-// Click Callback
+// ? Callback functions
+//  Click Callback
 const button = document.querySelector('.clickMe');
 function handleClick() {
     console.log('Clicked em!')
