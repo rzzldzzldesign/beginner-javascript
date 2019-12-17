d2b01dbfd93d69046db6eecea020355c9c777ca5
rzzldzzldesign <letsrazzledazzle@gmail.com>
2019-12-17 20:09:18 +1300

    Finished Tabs

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,35 @@
 console.log('ya ya wes we get it.. IT WORKS!');
+
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
+function handleTabClick(event) {
+  // HIDE ALL TAB PANELS
+  tabPanels.forEach(panel => {
+    panel.hidden = true;
+  });
+
+  // MARK AL TABS AS UNSELECTED
+  tabButtons.forEach(tab => {
+    tab.setAttribute('aria-selected', false);
+  });
+  // MARK THE CLICKED TAB AS SELECTED
+  event.currentTarget.setAttribute('aria-selected', true);
+  //  FIND THE ASSOCIATED TABPANEL AND SHOW IT
+  const { id } = event.currentTarget;
+
+  /*
+  METHOD ONE
+  const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+  tabPanel.hidden = false;
+  */
+
+  //   METHOD TWO
+  const tabPanel = tabPanels.find(
+    panel => panel.getAttribute('aria-labelledby') === id
+  );
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
