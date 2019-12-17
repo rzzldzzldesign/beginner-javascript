81c1802f606b485547d01f37a9d954faeb35217c
rzzldzzldesign <letsrazzledazzle@gmail.com>
2019-12-17 19:17:27 +1300

    Scroll to accept Done

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,23 @@
-console.log('IT WORKS!');
+const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
+
+function obCallback(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+  } else {
+    button.disabled = true;
+  }
+}
+
+const ob = new IntersectionObserver(obCallback, {
+  root: terms,
+  threshold: 1,
+});
+
+ob.observe(terms.lastElementChild);
+
+// terms.addEventListener('scroll', function(e) {
+//   console.log(e.currentTarget.scrollTop);
+//   console.log(e.currentTarget.scrollHeight);
+// });
